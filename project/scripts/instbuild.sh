7ddec21545a67d30987fd1f5865bf045fea86446
WilkinsonK <keenanwilkinson@outlook.com>
2022-11-06 09:24:08 -0600

    rearrange project

--- project/scripts/instbuild.sh
+++ project/scripts/instbuild.sh
@@ -4,21 +4,16 @@
 
 set -eu -o pipefail
 
-if [[ `whoami` != 'root' ]]; then
-    echo "notice: please run this script as root via sudo. exiting."
-    exit 1
-fi
-
 if [[ $# < 1 ]]; then
     echo "error: not enough args, expecting at least 1!"
     exit 2
 fi
 
-container_name=`printf 'postgres0x%2x' ${1}`
+container_name=`printf 'postgres0x%0 2x' ${1}`
 
 CONTAINER_IMAGE=postgres:latest
 CONTAINER_MOUNT_O1=type=bind,source=/mnt/postgres0x00,target=/var/lib/postgresql/data
-CONTAINER_MOUNT_O2=type=bind,readonly=true,source=/etc/passwd,target=/etc/passwd
+CONTAINER_MOUNT_O2=type=bind,source=/etc/passwd,target=/etc/passwd
 
 docker create --name $container_name \
     -p 5432:5432 \
